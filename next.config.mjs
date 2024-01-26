25ebf88e17f25c13a86124718215e263b88dde58
Omurbekova Tolkun <tolkun3922@gmail.com>
2024-01-26 13:56:33 +0600

    -configured project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  poweredByHeader:false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:4200/api/:path*`,
+      },
+      {
+        source: '/uploads/:path*',
+        destination: `http://localhost:4200/uploads/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
